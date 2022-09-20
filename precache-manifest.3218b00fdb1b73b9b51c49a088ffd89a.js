622a775de606277d6845a494e7221818f444b83d
Luigi Matias <luigi.matias@codehunter.com>
2022-09-19 23:04:44 -0400

    Updates

---
+++ precache-manifest.3218b00fdb1b73b9b51c49a088ffd89a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc05ca13918952a738ab337b39f6a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6759bd704ee8b12731cc",
+    "url": "/static/css/main.015e39d6.chunk.css"
+  },
+  {
+    "revision": "4a3c21bfc669063b5058",
+    "url": "/static/js/2.089be589.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.089be589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6759bd704ee8b12731cc",
+    "url": "/static/js/main.077b8b14.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b2154be6c94b86487e989db4fc9811a2",
+    "url": "/static/media/codehunter-dashboard.b2154be6.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "dba25ef314a7dde1ce6a1d0c3b6ff510",
+    "url": "/static/media/doodycalls.dba25ef3.png"
+  },
+  {
+    "revision": "2d9be9ab126af0f602c53fab0576809c",
+    "url": "/static/media/east-west.2d9be9ab.png"
+  },
+  {
+    "revision": "b6f17b739cbb36c8c82cc6e5bbb7e7aa",
+    "url": "/static/media/ncnk.b6f17b73.png"
+  },
+  {
+    "revision": "2ac13ceccfa5ce610ebce6ddaf68249d",
+    "url": "/static/media/rok.2ac13cec.png"
+  },
+  {
+    "revision": "5d3cd187e04dc873db661d98fdf756a2",
+    "url": "/static/media/vvmf.5d3cd187.png"
+  }
+]);
